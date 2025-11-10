6472076d73aabcbd147da5e409e58be4b80800a6
Darlitch <errorguy@mail.ru>
2025-11-10 13:46:46 +0700

    ver 0.851 add swagger

--- Search.Api/Program.cs
+++ Search.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Search.API.Middleware;
 using Search.Contract.Manager;
 using Search.Contract.Service;
@@ -10,6 +11,17 @@ builder.Services.AddHealthChecks();
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "Search API", 
+        Version = "v1",
+        Description = "API для работы с Search"
+    });
+});
+
 builder.Services.AddHttpClient<IUserSearchService, UserSearchService>(client =>
 {
     client.BaseAddress = new Uri("http://user-service:8080");
@@ -20,6 +32,15 @@ builder.Services.AddScoped<IUserSearchManager, UserSearchManager>();
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Search v1");
+        c.RoutePrefix = "swagger";
+    });
+}
 app.UseRouting();
 app.MapControllers();
 app.MapHealthChecks("/health");
